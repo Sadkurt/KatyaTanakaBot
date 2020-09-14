cec812bd47e3c034c8592855a212931ae2763623
sadkurt <Dauvaslambert2>
2020-09-14 16:22:33 +0300

    danborchik

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import os
 import random
 from uuid import uuid4
 
-from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineQueryResultPhoto, InlineQueryResultGif, Bot
+from telegram import InlineQueryResultArticle, InputTextMessageContent,\
+                     InlineQueryResultPhoto, InlineQueryResultGif, Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
 from pybooru import Danbooru
 
@@ -14,18 +15,37 @@ PORT = int(os.environ.get('PORT', 5000))
 TOKEN = os.environ["TOKEN"]
 DUSERNAME = os.environ["DUSERNAME"]
 DAPIKEY = os.environ["DAPIKEY"]
+
+updater = Updater(TOKEN, use_context=True)
 client = Danbooru('danbooru', username=DUSERNAME, api_key=DAPIKEY)
 bot = Bot(TOKEN)
 
 
-# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
+
+def main():
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("random", random_command))
+    dp.add_handler(CommandHandler("gif", gif_command))
+    dp.add_handler(InlineQueryHandler(choose))
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://tgbotkatya.herokuapp.com/' + TOKEN)
+
+    updater.idle()
+
+
 def help_command(update, context):
-    update.message.reply_text('Я вхожу в данбору без стука! \danb моя команда - шнырь!')
+    update.message.reply_text('Я вхожу в данбору без стука!  моя команда - шнырь!')
 
 
 def random_command(update, context):
@@ -114,6 +134,7 @@ def fetch_random_post():
                     photo_url=message))
     return results
 
+
 def choose(update, context):
     query = update.inline_query.query
     if query == "random":
@@ -123,37 +144,7 @@ def choose(update, context):
     else:
         tags = client.tag_list(name_matches=query + '*')
         results = fetch_posts_by_tags(tags)
-    update.inline_query.answer(results,cache_time=10)
-
-def danb(update, context):
-    posts = client.post_list(tags=str(context.args[0]), limit=1)
-    if not posts:
-        update.message.reply_text("Пустой запрос")
-    else:
-        for post in posts:
-            if "file_url" in post:
-                update.message.reply_text(str(post["large_file_url"]))
-            else:
-                update.message.reply_text("Нету ссылки или забанен")
-
-
-def main():
-
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("help", help_command))
-    dp.add_handler(CommandHandler("random", random_command))
-    dp.add_handler(CommandHandler("gif", gif_command))
-    # dp.add_handler(CommandHandler("danb", danb, pass_args=True))
-    dp.add_handler(InlineQueryHandler(choose))
-
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://tgbotkatya.herokuapp.com/' + TOKEN)
-
-    updater.idle()
+    update.inline_query.answer(results, cache_time=1)
 
 
 if __name__ == '__main__':
